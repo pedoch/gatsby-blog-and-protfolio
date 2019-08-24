8c8b1d78458a3c8684bd3ed90a4a504c43562200
unknown <pedroogheneochuko@gmail.com>
2019-08-24 18:20:51 +0100

    continuing with blog
    -Dynamically creating blog pages for every md file created
    -Linked md file contents to blog post template

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,11 +1,48 @@
 import React from "react"
+import { useStaticQuery, graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 
 const BlogPage = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            frontmatter {
+              title
+              date
+            }
+            html
+            excerpt
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+
   return (
     <Layout>
-      <h1>My Blog</h1>
-      <p>Post will show up hee later on</p>
+      <h2>My Blog</h2>
+      <ol>
+        {data.allMarkdownRemark.edges.map((post, index) => {
+          return (
+            <li
+              key={
+                post.node.frontmatter.title + index + post.node.frontmatter.date
+              }
+            >
+              <Link to={`/blog/${post.node.fields.slug}`}>
+                <h4>{post.node.frontmatter.title}</h4>
+                <h6>{post.node.excerpt}</h6>
+                <p>{post.node.frontmatter.date}</p>
+              </Link>
+            </li>
+          )
+        })}
+      </ol>
     </Layout>
   )
 }
--- src/posts/react.md
+++ src/posts/react.md
@@ -3,4 +3,4 @@ title: "React Unlocked!"
 date: "18-08-2019"
 ---
 
-Let's what React's got!
+Let's see what React's got!
