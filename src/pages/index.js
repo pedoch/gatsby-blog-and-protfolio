798925563a5e49ec9bb025b4bffd0ffccc88063f
unknown <pedroogheneochuko@gmail.com>
2019-08-27 05:35:12 +0100

    Clean up home page links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Head from "../components/head"
 
@@ -9,12 +8,6 @@ const HomePage = () => {
       <Head title="Home" />
       <h1>Hello.</h1>
       <h2>I'm Pedro, a front-end developer living in Nigeria</h2>
-      <p>
-        Need a developer? <a href="/contact">Contact Me</a>.
-      </p>
-      <p>
-        Need a developer? <Link to="/contact">Contact Me</Link>.
-      </p>
     </Layout>
   )
 }
